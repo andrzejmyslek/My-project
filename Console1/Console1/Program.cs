fdb8e552dda8e0754674a9328f85be5f47242ef7
andrzejmyslek <andrzej.myslek@globallogic.com>
2017-09-07 11:46:08 +0200

    first commit

---
+++ Console1/Console1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: andrzej.myslek
+ * Date: 9/7/2017
+ * Time: 11:43 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Console1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
