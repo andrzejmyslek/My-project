9ec1b6a7ab22ecabc41f7724af02d0391c691c47
andrzejmyslek <andrzej.myslek@globallogic.com>
2017-09-07 11:49:45 +0200

    dsa

--- Console1/Console1/Program.cs
+++ Console1/Console1/Program.cs
@@ -19,6 +19,7 @@ namespace Console1
 			Console.WriteLine("Hello World2!");
 			Console.WriteLine("Hello World3!");
 			Console.WriteLine("Hello World4!");
+			Console.WriteLine("Hello World5!");
 			
 			// TODO: Implement Functionality Here
 			
